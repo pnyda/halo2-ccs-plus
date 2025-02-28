a54a2d99fbe171517c07580352de1f4a9fd147e5
pnyda <135603985+pnyda@users.noreply.github.com>
2025-02-28 15:16:47 +0900

    check_lookup_satisfiability

--- src/lookup.rs
+++ src/lookup.rs
@@ -11,19 +11,52 @@ use std::ops::Div;
 use std::ops::Mul;
 use std::ops::Sub;
 
-fn check_lookup_satisfiability<F: PrimeField>(k: u32, subset: Vec<F>, superset: Vec<F>) -> bool {
+pub(crate) fn check_lookup_satisfiability<F: PrimeField>(
+    k: u32,
+    subset: Vec<F>,
+    superset: Vec<F>,
+) -> bool {
     let (subset_sorted, superset_sorted) = crate::lookup::sort(k, &subset, &superset);
+    let is_multiset_equal =
+        check_multiset_equality(&superset, &superset_sorted, &subset_sorted, &subset);
+    let is_copying_this_or_that = copy_this_or_that(&superset_sorted, &subset_sorted);
 
-    let evaluation_domain = Radix2EvaluationDomain::new(1 << k).unwrap();
-    // A(X), S(X), A'(X), S'(X) in the Halo2 book
-    // https://zcash.github.io/halo2/design/proving-system/lookup.html
-    let a = Evaluations::from_vec_and_domain(subset, evaluation_domain).interpolate();
-    let s = Evaluations::from_vec_and_domain(superset, evaluation_domain).interpolate();
-    let a_prime = Evaluations::from_vec_and_domain(subset_sorted, evaluation_domain).interpolate();
-    let a_prime =
-        Evaluations::from_vec_and_domain(superset_sorted, evaluation_domain).interpolate();
+    is_multiset_equal && is_copying_this_or_that
+}
 
-    false
+fn copy_this_or_that<F: PrimeField + FftField>(superset_sorted: &[F], subset_sorted: &[F]) -> bool {
+    assert_eq!(subset_sorted.len(), superset_sorted.len());
+    assert!(subset_sorted.len().is_power_of_two());
+    let k = log2(subset_sorted.len());
+
+    let original_domain = Radix2EvaluationDomain::new(1 << k).unwrap();
+    // We need to expand the evaluation domain first, because we'll do polynomial multiplications later.
+    // We'll need to handle polynomials with larger degrees.
+    let expanded_domain = Radix2EvaluationDomain::new(1 << k << 1).unwrap();
+
+    let mut subset_sorted = subset_sorted.to_vec();
+    let A_prime = Evaluations::from_vec_and_domain(subset_sorted.clone(), original_domain)
+        .interpolate()
+        .evaluate_over_domain(expanded_domain);
+    subset_sorted.rotate_right(1);
+    let A_prime_shifted = Evaluations::from_vec_and_domain(subset_sorted, original_domain)
+        .interpolate()
+        .evaluate_over_domain(expanded_domain);
+    let S_prime = Evaluations::from_vec_and_domain(superset_sorted.to_vec(), original_domain)
+        .interpolate()
+        .evaluate_over_domain(expanded_domain);
+
+    let mut lagrange0: Vec<F> = vec![0.into(); expanded_domain.size()];
+    lagrange0[0] = 1.into();
+    let lagrange0 = Evaluations::from_vec_and_domain(lagrange0, expanded_domain);
+
+    // (A'(X) - S'(X)) * (A'(X) - A'(X / omega)) = 0
+    let constraint1 = A_prime.sub(&S_prime).mul(&A_prime.sub(&A_prime_shifted));
+    // l_0(X) * (A'(X) - S'(X))
+    let constraint2 = A_prime.sub(&S_prime).mul(&lagrange0);
+
+    zero_test(&constraint1.interpolate(), original_domain)
+        && zero_test(&constraint2.interpolate(), original_domain)
 }
 
 // a_prime must be multiset-equal to a.
@@ -42,11 +75,12 @@ fn check_multiset_equality<F: UniformRand + PrimeField + FftField>(
     let k = log2(subset.len());
 
     // Random challenges.
+    // Maybe TODO: fiat shamir?
     let beta = F::rand(&mut OsRng);
     let gamma = F::rand(&mut OsRng);
 
     // Note that Z in Halo2 book is different from the witness vector Z in CCS
-    // Z works as a proof a proof of multiset equality
+    // Z works as a proof of multiset equality
     let mut Z: Vec<F> = vec![1.into()];
 
     for i in 0..subset.len() {
@@ -219,5 +253,51 @@ mod tests {
             superset_sorted,
             vec![1.into(), 4.into(), 2.into(), 2.into()]
         );
+    }
+
+    #[test]
+    fn test_multiset_equality_success() {
+        let subset: [Fq; 4] = [2.into(), 2.into(), 1.into(), 1.into()];
+        let superset: [Fq; 4] = [4.into(), 2.into(), 1.into(), 2.into()];
+        let subset_sorted: [Fq; 4] = [1.into(), 1.into(), 2.into(), 2.into()];
+        let superset_sorted: [Fq; 4] = [1.into(), 4.into(), 2.into(), 2.into()];
+
+        assert!(super::check_multiset_equality(
+            &superset,
+            &superset_sorted,
+            &subset_sorted,
+            &subset
+        ))
+    }
+
+    #[test]
+    fn test_multiset_equality_fail() {
+        let subset: [Fq; 4] = [1.into(), 2.into(), 1.into(), 1.into()];
+        let subset_sorted: [Fq; 4] = [1.into(), 1.into(), 2.into(), 2.into()];
+        let superset: [Fq; 4] = [4.into(), 2.into(), 1.into(), 2.into()];
+        let superset_sorted: [Fq; 4] = [1.into(), 4.into(), 2.into(), 2.into()];
+
+        assert!(!super::check_multiset_equality(
+            &superset,
+            &superset_sorted,
+            &subset_sorted,
+            &subset
+        ))
+    }
+
+    #[test]
+    fn test_copy_this_or_that_success() {
+        let superset_sorted: [Fq; 4] = [0.into(), 1.into(), 2.into(), 3.into()];
+        let subset_sorted: [Fq; 4] = [0.into(), 0.into(), 2.into(), 3.into()];
+
+        assert!(super::copy_this_or_that(&superset_sorted, &subset_sorted))
+    }
+
+    #[test]
+    fn test_copy_this_or_that_fail() {
+        let superset_sorted: [Fq; 4] = [0.into(), 1.into(), 3.into(), 2.into()];
+        let subset_sorted: [Fq; 4] = [0.into(), 0.into(), 2.into(), 3.into()];
+
+        assert!(!super::copy_this_or_that(&superset_sorted, &subset_sorted))
     }
 }
